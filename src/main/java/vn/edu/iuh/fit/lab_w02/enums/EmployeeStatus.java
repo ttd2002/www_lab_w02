432ec11580762c09d30fe316a77d556fb393e9c5
Student <thanhdai912@gmail.com>
2023-09-24 08:23:01 +0700

    add2

--- src/main/java/vn/edu/iuh/fit/lab_w02/enums/EmployeeStatus.java
+++ src/main/java/vn/edu/iuh/fit/lab_w02/enums/EmployeeStatus.java
@@ -1,4 +1,14 @@
 package vn.edu.iuh.fit.lab_w02.enums;
 
-public class EmployeeStatus {
+public enum EmployeeStatus {
+    ACTIVE(1),
+    IN_ACTIVE(0),
+    TERMINATED(-1);
+    private  int value;
+    EmployeeStatus(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
 }
--- src/main/java/vn/edu/iuh/fit/lab_w02/models/Customer.java
+++ src/main/java/vn/edu/iuh/fit/lab_w02/models/Customer.java
@@ -1,4 +1,82 @@
 package vn.edu.iuh.fit.lab_w02.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cust_id")
+    private long id;
+    @Column(name = "cust_name", length = 150, nullable = false)
+    private String name;
+    @Column(name = "gmail", unique = true, length = 150, nullable = true)
+    private String gmail;
+    @Column(name = "phone", length = 15, nullable = false)
+    private String phone;
+    @Column(name = "address", length = 250, nullable = false)
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(long id, String name, String gmail, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.gmail = gmail;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gmail='" + gmail + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
